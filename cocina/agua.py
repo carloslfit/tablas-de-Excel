22f2d4c11ccf4d5e0f5609ed4e2f33c9da5eea65
carloslfit <arturolf06@gmail.com>
2026-02-27 08:50:52 -0500

    Delete cocina directory

--- cocina/agua.py
+++
@@ -1,9 +0,0 @@
-class Agua:
-    def __init__(self):
-        self.hirviendo = False
-        
-    def hervir(self):
-        self.hirviendo =True
-        
-    def mostrar_info(self):
-        return (f"Agua hirviendo: {self.hirviendo}")
